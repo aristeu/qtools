c4eae9917af1f32e1c0ecc34abcb5bfc960302b1
Aristeu Rozanski <arozansk@redhat.com>
2023-07-25 15:09:03 -0400

    qimport: support standard input
    This is huge but necessary refactoring to allow more features that are
    planned.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- qimport.py
+++ qimport.py
@@ -17,7 +17,7 @@ def write_single_patch(path, series, commit):
     patch = "%s.patch" % commit
     f = open("patches/%s" % patch, "w")
     # ugh. Couldn't make commit.diff work the same way
-    formatted_patch = subprocess.check_output(["git", "-C", path, "log", "-p", "-1", commit])
+    formatted_patch = subprocess.check_output(["git", "-C", path, "log", "-p", "-1", commit.hexsha])
     # ffs
     try:
         f.write(formatted_patch.decode('utf-8'))
@@ -32,6 +32,7 @@ def list_patchset(repo, branch, interval, commit):
     date = commit.committed_date
 
     commit_list = []
+    commit_sha_list = []
     found_commit = False
     for c in repo.iter_commits(branch):
         if c.committed_date < (date - interval):
@@ -41,21 +42,23 @@ def list_patchset(repo, branch, interval, commit):
             if c.hexsha == commit.hexsha:
                 found_commit = True
             if c.author.email == author_email:
-                commit_list.append(c.hexsha)
+                commit_sha_list.append(c.hexsha)
+                commit_list.append(c)
             else:
                 if found_commit:
                     # we did it,
                     break
                 # author might have changed and we didn't find the target commit
                 commit_list.clear()
+                commit_sha_list.clear()
 
     if len(commit_list) == 0:
         sys.stderr.write("Commit %s not found in branch %s. Specify a different repo or branch\n" % (commit, branch))
         sys.stderr.write("Until an option is implemented to do this automatically, run 'git branch -a --contains %s'\n" % commit)
         return []
 
-    commit_list.reverse()
-    return commit_list
+    commit_sha_list.reverse()
+    return (commit_list, commit_sha_list)
 
 def import_patches(repo, path, commit_list):
     try:
@@ -76,18 +79,13 @@ def import_patches(repo, path, commit_list):
         raise("Unable to open series file: %s\n" % str(ex))
 
     lines = series.readlines()
-    for c in commit_list:
-        commit = repo.commit(c)
-        patch = "%s.patch" % c
+    for commit in commit_list:
+        patch = "%s.patch" % commit.hexsha
         if ("%s\n" % patch) in lines:
             print("Skipping %s: already in series" % patch)
             continue
-        write_single_patch(path, series, c)
+        write_single_patch(path, series, commit)
     series.close()
-
-def import_single_patch(repo, path, sha):
-    commit_list = [ sha ]
-    import_patches(repo, path, commit_list)
 
 def get_commit_from_file(c):
     commit = c.replace('.patch', '')
@@ -136,11 +134,11 @@ def check_series(repo, branch, interval):
         if commit is None:
             continue
 
-        l = list_patchset(repo, branch, interval, commit)
-        if len(l) == 0:
+        (commit_list, commit_sha_list) = list_patchset(repo, branch, interval, commit)
+        if len(commit_sha_list) == 0:
             return 1
 
-        for p in l:
+        for p in commit_sha_list:
             if p not in lines:
                 if p not in missing_list:
                     missing_list.append(p)
@@ -155,7 +153,7 @@ def usage(f, name):
     f.write("%s <commit>\n" % name)
 
 def main(argv):
-    usage = "usage: %prog [options] <commit sha>"
+    usage = "usage: %prog [options] <commit sha | ->"
     parser = optparse.OptionParser(usage=usage)
     parser.add_option("-g", "--git", dest="git", default=None, help="Specify an alternate git repository path instead of the configuration one", metavar="GIT")
     parser.add_option("-P", "--patchset", dest="patchset", default=False, help="Import the whole patchset the specified commit belongs to", action="store_true")
@@ -209,28 +207,57 @@ def main(argv):
     if check:
         return check_series(repo, branch, interval)
 
-    commit_sha = args[0]
-    try:
-        commit = repo.commit(commit_sha)
-    except:
-        sys.stderr.write("Unable to find commit %s in repository %s\n" % (commit_sha, path))
-        sys.exit(1)
+    commit_sha_list = []
+    commit_list = []
 
-    if not patchset and not list_only:
-        import_single_patch(repo, path, commit.hexsha)
-        return 0
+    # handle standard input
+    if args[0] == '-':
+        for item in sys.stdin.read().split('\n'):
+            if len(item.replace(' ', '')) == 0:
+                continue
+            commit_sha_list.append(item)
 
-    commit_list = list_patchset(repo, branch, interval, commit)
+    else:
+        commit_sha_list = args[0].split(',')
+
+    # we have the user provided list, now gather commits objects
+    for commit_sha in commit_sha_list:
+        try:
+            commit = repo.commit(commit_sha)
+            commit_list.append(commit)
+        except Exception as ex:
+            sys.stderr.write("Unable to find commit %s in repository %s\n" % (commit_sha, path))
+            # TODO: add a force option to ignore
+            sys.exit(1)
+
+    # now, if asked, add patches in the same patchset
+    output_sha_list = []
+    output_list = []
+    if patchset or list_only:
+        for commit in commit_list:
+            (clist, sha_list) = list_patchset(repo, branch, interval, commit)
+            for sha in sha_list:
+                if sha not in output_sha_list:
+                    output_sha_list.append(sha)
+            for c in clist:
+                if c not in output_list:
+                    output_list.append(c)
+
+        # list_patchset will expand the commit id automatically, no need to redo it
+        commit_sha_list = output_sha_list
+        commit_list.extend(output_list)
+
     if len(commit_list) == 0:
         sys.exit(1);
 
+    # only listing
     if list_only:
-        for c in commit_list:
+        for c in commit_sha_list:
             print(c)
         return 0
 
-    if do_import:
-        import_patches(repo, path, commit_list)
+    # applying
+    import_patches(repo, path, commit_list)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
